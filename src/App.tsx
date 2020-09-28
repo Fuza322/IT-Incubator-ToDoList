9186848637fce1336d69acf0bf4c09d0ee20e202
Fuza322 <vanya.osipik322@gmail.com>
2020-09-28 17:53:15 +0300

    Lesson2

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Todolist} from "./Todolist";
+import {Todolist} from './Todolist';
 
 export type TaskType = {
     id: number
     title: string
     isDone: boolean
 }
+export type FilerValuesType = 'all' | 'completed' | 'active'
 
 function App() {
+    let [initTasks, setTasks] = useState <Array<TaskType>> (
+        [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: false},
+        {id: 3, title: 'ReactJS', isDone: true},
+        {id: 4, title: 'TypeScript', isDone: true},
+        {id: 5, title: 'CodeWars', isDone: false}
+        ]
+    )
 
-    let tasks1: Array<TaskType> = [
-        {id:1, title:"HTML&CSS", isDone: true},
-        {id:2, title:"JS", isDone: true},
-        {id:3, title:"ReactJS", isDone: false}
-    ]
+    let [filerTasks, setFilterTasks] = useState <FilerValuesType> ('all')
 
-    let tasks2: Array<TaskType> = [
-        {id:1, title:"Hello, world", isDone: true},
-        {id:2, title:"I am Happy", isDone: false},
-        {id:3, title:"Yo", isDone: true}
-    ]
+    if (filerTasks == "completed") {
+        initTasks = initTasks.filter(t => t.isDone == true)
+    }
+    if (filerTasks == "active") {
+        initTasks = initTasks.filter(t => t.isDone == false)
+    }
 
+    function removeTask(id: number) {
+        initTasks = initTasks.filter((t) => t.id !== id)
+        setTasks(initTasks)
+    }
 
+    function changeFilter(value: FilerValuesType) {
+        setFilterTasks(value)
+    }
 
     return (
-        <div className="App">
-            <Todolist title="What to learn" tasks={tasks1}/>
+        <div className='App'>
+            <Todolist
+                title='What to learn'
+                tasks={initTasks}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+            />
         </div>
     );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import {TaskType} from "./App";
+import React from 'react';
+import {FilerValuesType, TaskType} from './App';
 
-type PropsType = {
+export type PropsType = {
     title: string
     tasks: Array<TaskType>
+    removeTask: (taskID: number) => void
+    changeFilter: (value: FilerValuesType) => void
 }
 
 export function Todolist(props: PropsType) {
     return (
-        <div className="App">
+        <div className='App'>
             <div>
                 <h3>{props.title}</h3>
                 <div>
@@ -17,20 +19,21 @@ export function Todolist(props: PropsType) {
                 </div>
                 <ul>
                     {
-                        props.tasks.map(task => {
+                        props.tasks.map(t => {
                             return (
-                                <li key={task.id}>
-                                    <input type="checkbox" checked={task.isDone}/>
-                                    <span>{task.title}</span>
+                                <li key={t.id}>
+                                    <input type='checkbox' checked={t.isDone}/>
+                                    <span>{t.title}</span>
+                                    <button onClick={ () => { props.removeTask(t.id) } }>x</button>
                                 </li>
                             )
                         })
                     }
                 </ul>
                 <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
+                    <button onClick={() => {props.changeFilter('all')}}>All</button>
+                    <button onClick={() => {props.changeFilter('active')}}>Active</button>
+                    <button onClick={() => {props.changeFilter('completed')}}>Completed</button>
                 </div>
             </div>
         </div>
